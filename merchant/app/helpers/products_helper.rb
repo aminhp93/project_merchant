a154fa0a5283188d9831971faebf61c0df755c52
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-21 02:58:05 -0700

    Adding to the Product Listing

--- merchant/app/helpers/products_helper.rb
+++ merchant/app/helpers/products_helper.rb
@@ -3,4 +3,15 @@ module ProductsHelper
 		# format("$%.2f", price)
 		number_to_currency price
 	end
+
+	def print_stock(stock)
+		if stock > 0
+			# <span class="in_stock">In Stock (##)</span>
+			content_tag(:span, "In Stock (##)", class: "in_stock")
+
+		else
+			# <span class="out_stock">Out of Stock </span>
+			content_tag(:span, "Out of Stock", class: "out_stock")
+		end
+	end
 end
