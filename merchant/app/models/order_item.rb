8e5bb3a76e35da1405b3dd2a127951b26d647a51
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-24 16:58:35 -0700

    OrderItem Subtotal

--- merchant/app/models/order_item.rb
+++ merchant/app/models/order_item.rb
@@ -3,5 +3,10 @@ class OrderItem < ActiveRecord::Base
 	belongs_to :product
 	
 	validates_presence_of :order_id, :product_id
+	validates :quantity, numericality: {only_integer: true, greater_than: 0}
+
+	def subtotal
+		quantity * product.price
+	end
 
 end
