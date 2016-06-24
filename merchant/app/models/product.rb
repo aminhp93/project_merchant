523e1544aa05f0e75872bd804fc42dba10353a47
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-24 01:33:41 -0700

    Making the Stock Editable

--- merchant/app/models/product.rb
+++ merchant/app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
 	validates_numericality_of :price
+	validates_numericality_of :stock, :message=> "please insert a number", :only_integer => true, :greater_than_or_equal_to => 0
 
 	def price=(input)
 		# self.price = input
