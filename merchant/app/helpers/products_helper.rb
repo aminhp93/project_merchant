b145d6759cb6362fed577cf07d87b76c05ac87a7
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-25 16:02:38 -0700

    Displaying Stock on the Order Page

--- merchant/app/helpers/products_helper.rb
+++ merchant/app/helpers/products_helper.rb
@@ -4,14 +4,15 @@ module ProductsHelper
 		number_to_currency price
 	end
 
-	def print_stock(stock)
-		if stock > 0
+	def print_stock(stock, requested = 1)
+		if stock == 0
 			# <span class="in_stock">In Stock (##)</span>
-			content_tag(:span, "In Stock (##)", class: "in_stock")
-
-		else
-			# <span class="out_stock">Out of Stock </span>
 			content_tag(:span, "Out of Stock", class: "out_stock")
+		elsif stock >= requested
+			content_tag(:span, "In Stock (##)", class: "in_stock")
+		else
+			content_tag(:span, "Insufficient stock (#{stock})", class: "low_stock")
+			# <span class="out_stock">Out of Stock </span>
 		end
 	end
 end
