6f1aa92058a2c9252282760d58fcb1b076e2bbdb
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-25 16:27:30 -0700

    Finding or Creating User Objects

--- merchant/app/controllers/sessions_controller.rb
+++ merchant/app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
 	def create 
 		render text: request.env["omniauth.auth"]
+		@user = User.find_or_create_by_auth(request.env["omniauth"])
 	end
 end
--- merchant/app/models/user.rb
+++ merchant/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+	def sefl.find_or_create_by_auth(auth_data)
+		# find_or_create_by_provider_and_uid_and_name(auth_data["provider"], auth_data["uid"], auth_data["info"]["name"])
+		# find_or_create_by_provider_and_uid(auth_data["provider"], auth_data["uid"])
+		find_or_create_by_provider_and_uid(auth_data["provider"], auth_data["uid"], name: auth_data["info"]["name"])
+	end
 end
