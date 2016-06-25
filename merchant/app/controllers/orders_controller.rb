adfe76eb54347a9c0461ff3ce2c82e43367fbc0c
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-25 11:40:00 -0700

    Clearing All Items from an Order

--- merchant/app/controllers/orders_controller.rb
+++ merchant/app/controllers/orders_controller.rb
@@ -59,6 +59,7 @@ class OrdersController < ApplicationController
       format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
       format.json { head :no_content }
     end
+    # redirect_to product_path
   end
 
   private
