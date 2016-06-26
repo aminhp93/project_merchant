4c372ea4ff2d7482a6da57600595922b1c321d8b
aminhp93 <minhpn.org.ec@gmail.com>
2016-06-25 21:22:33 -0700

    Convenivent Routes

--- merchant/config/routes.rb
+++ merchant/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  match "/login" => redirect("/auth/twitter"), as: :login, via: :get
+  match "/logout" => "sessions#destroy", as: :logout, via: :get
   match '/auth/:provider/callback', to: 'sessions#create', via: :get
 
   resources :orders
